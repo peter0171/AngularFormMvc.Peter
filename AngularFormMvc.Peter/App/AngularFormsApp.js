e95e4a0c91dc9578137d78d3aa1b2c8104a18019
peteroneshift <peter@oneshift.com.au>
2015-11-12 21:59:12 +1100

    1211-Add angular module,route ,controller.

---
+++ AngularFormMvc.Peter/App/AngularFormsApp.js
@@ -0,0 +1,23 @@
+﻿
+var App = angular.module('angularFormsApp', ["ngRoute"]);
+
+App.config(["$routeProvider", "$locationProvider",
+    function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when("/home", {
+            templateUrl: "App/Home.html",
+            controller: "HomeController"
+        })
+        .otherwise({
+            redirectTo: "/home"
+        });
+    //$locationProvider.html5Mode(true);
+}]);
+
+App.controller("HomeController",
+    ["$scope", "$location", 
+    function ($scope, $location) {
+  
+
+    }]);
+
